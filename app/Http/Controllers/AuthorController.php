dede54d66ff3f5d04a20e4543abd66d7219bf9d5
Lexi Cummins <lexi@rmaster.com>
2020-01-17 16:22:44 -0500

    author & book controllers added

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    use ApiResponser;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of authors.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create one new author.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Obtain & show details of one author.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+
+    }
+
+    /**
+     * Update existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+
+    }
+    /**
+     * Remove existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+   use ApiResponser;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of books.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create one new book.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Obtain & show details of one book.
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($book)
+    {
+
+    }
+
+    /**
+     * Update existing book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+
+    }
+    /**
+     * Remove existing book
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($book)
+    {
+
+    }
+}
